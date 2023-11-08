56cedd7a6536cd85535ed6e1cbcc31f29ccb98d9
Adil Awan <tritechuzaifa@gmail.com>
2023-11-08 10:22:23 +0500

    Adil

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,6 +1,5 @@
 import 'package:get/get.dart';
 import 'package:mudarribe_trainee/routes/app_routes.dart';
-import 'package:mudarribe_trainee/views/authentication/signin/view.dart';
 import 'package:mudarribe_trainee/views/authentication/signup/signup_binding.dart';
 import 'package:mudarribe_trainee/views/authentication/signup/signup_view.dart';
 import 'package:mudarribe_trainee/views/onboardings/onboarding_view.dart';
