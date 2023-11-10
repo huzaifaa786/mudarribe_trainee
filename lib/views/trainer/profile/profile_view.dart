fa797372b81cd9983874ee5076b9fb7d3a7e2d54
usama <tritechuzaifa@gmail.com>
2023-11-10 11:18:33 +0500

    remove text

--- lib/views/trainer/profile/profile_view.dart
+++ lib/views/trainer/profile/profile_view.dart
@@ -265,7 +265,7 @@ class _TrainerprofileViewState extends State<TrainerprofileView> {
                           },
                         ),
                       ),
-                      Text('asdfg'),
+                      
                     ],
                   ),
                 ),
