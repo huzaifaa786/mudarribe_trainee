56a7d411bca74f6e773e71ca923be28a780b7159
Haseeb <tritechuzaifa@gmail.com>
2023-11-11 16:49:38 +0500

    Home Controller Functionality

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -8,6 +8,8 @@ import 'package:mudarribe_trainee/views/authentication/signup/signup_binding.dar
 import 'package:mudarribe_trainee/views/authentication/signup/signup_view.dart';
 import 'package:mudarribe_trainee/views/catigories/catigories_result_screen.dart';
 import 'package:mudarribe_trainee/views/events/eventsdetail/eventsDetail_view.dart';
+import 'package:mudarribe_trainee/views/home/home_binding.dart';
+import 'package:mudarribe_trainee/views/home/home_view.dart';
 import 'package:mudarribe_trainee/views/onboardings/onboarding_view.dart';
 import 'package:mudarribe_trainee/views/splash/splash_binding.dart';
 import 'package:mudarribe_trainee/views/splash/splash_view.dart';
@@ -25,15 +27,19 @@ class AppPages {
       page: () => const OnBoardingView(),
     ),
     GetPage(
+      name: AppRoutes.home,
+      page: () => const HomeView(),
+      binding: HomeBinding(),
+    ),
+    GetPage(
       name: AppRoutes.signup,
       page: () => const SignUpView(),
       binding: SignupBinding(),
     ),
-        GetPage(
+    GetPage(
       name: AppRoutes.catigories,
       page: () => const CatigoriesResultScreen(),
-        ),
-     
+    ),
     GetPage(
       name: AppRoutes.trainerprofile,
       page: () => const TrainerprofileView(),
--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -3,6 +3,8 @@ class AppRoutes {
   static const onBoarding = '/onBoarding';
   static const signup = '/signup';
   static const catigories = '/catigories';
+  static const home = '/home';
+
 
   static const signin = '/signin';
   static const forgot = '/forgot';
